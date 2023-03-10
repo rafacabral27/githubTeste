9fc9932f6f7b5643a278fa19d487a18430a1e37e
rafacabral27 <126591479+rafacabral27@users.noreply.github.com>
2023-03-10 15:40:56 +0000

    teste feito

--- src/main/java/com/mycompany/teste/Teste.java
+++ src/main/java/com/mycompany/teste/Teste.java
@@ -11,6 +11,6 @@ package com.mycompany.teste;
 public class Teste {
 
     public static void main(String[] args) {
-        System.out.println("Hello my friend!");
+        System.out.println("Hello, my name is Rafael!");
     }
 }
